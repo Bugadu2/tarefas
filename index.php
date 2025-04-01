03dc958c21898ebdb8a04bffa0bc0471aab78f3f
joao.nogueira <joaonogueira20181@gmail.com>
2025-04-01 15:41:24 -0300

    agora vai

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
- 
-use PhpParser\Node\Stmt\While_;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpNotFoundException;
 use Slim\Factory\AppFactory;
- 
+
 require __DIR__ . '/vendor/autoload.php';
- 
+
 $app = AppFactory::create();
- 
+
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 $errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (
     Request $request,
-    Throwable $expection,
-    bool $diplayErrorDetails,
+    Throwable $exception,
+    bool $displayErrorDetails,
     bool $logErrors,
     bool $logErrorDetails
 ) use ($app) {
@@ -22,13 +21,13 @@ $errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (
     $response->getBody()->write('{"error": "voce ser carente!"}');
     return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
 });
- 
+
 $app->get('/ola/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
     return $response;
 });
- 
+
 $app->get('/tarefas', function (Request $request, Response $response, array $args) {
     $tarefas = [
         ["id" => 1, "titulo" => "Veja que legal1", "concluido" => false],
@@ -39,37 +38,36 @@ $app->get('/tarefas', function (Request $request, Response $response, array $arg
     $response->getBody()->write(json_encode($tarefas));
     return $response->withHeader('Content-Type', 'application/json');
 });
- 
-$app->post('/tarefas', function(Request $request, Response $response, array $args) {
-   $parametros = (array) $request->getParsedBody();
-   if(!array_key_exists('titulo', $parametros) ||  empty($parametros ['titulo'])){
-    $response->getBody()->write(json_encode([
-        "mensagem" => "titulo e obrigatorio"
-    ]));
-    return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
-   }
+
+$app->post('/tarefas', function (Request $request, Response $response, array $args) {
+    $parametros = (array) $request->getParsedBody();
+    if (!array_key_exists('titulo', $parametros) || empty($parametros['titulo'])) {
+        $response->getBody()->write(json_encode([
+            "mensagem" => "titulo e obrigatorio"
+        ]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+    }
     return $response->withStatus(201);
 });
- 
-$app->delete('/tarefas', function(Request $request, Response $response, array $args){
+
+$app->delete('/tarefas', function (Request $request, Response $response, array $args) {
     return $response->withStatus(204);
 });
- 
-$app->put('/tarefas', function(Request $request, Response $response, array $args){
+
+$app->put('/tarefas', function (Request $request, Response $response, array $args) {
     return $response->withStatus(204);
 });
- 
-$app->put('/tarefas/{id}', function (Request $request, Reponse $response, array $args) {
+
+$app->put('/tarefas/{id}', function (Request $request, Response $response, array $args) {
     $id = $args['id'];
     $dados_para_atualizar = (array) $request->getParsedBody();
-    if(array_key_exists('titulo', $dados_para_atualizar) && empty($dados_para_atualizar['titulo'])){
+    if (array_key_exists('titulo', $dados_para_atualizar) && empty($dados_para_atualizar['titulo'])) {
         $response->getBody()->write(json_encode([
             "mensagem" => "titulo é obrigatorio"
         ]));
-            return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
     }
-    return $response->withStatus(201)
-
+    return $response->withStatus(201);
 });
 
 $app->run();
