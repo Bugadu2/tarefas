d18c64dd2ab49b1bb2449a32ca186e1c9e8b9316
joao.nogueira <joaonogueira20181@gmail.com>
2025-04-01 15:44:31 -0300

    dale

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $app->put('/tarefas/{id}', function (Request $request, Response $response, array
     $dados_para_atualizar = (array) $request->getParsedBody();
     if (array_key_exists('titulo', $dados_para_atualizar) && empty($dados_para_atualizar['titulo'])) {
         $response->getBody()->write(json_encode([
-            "mensagem" => "titulo é obrigatorio"
+            "error" => "Login e senha são obrigatorios"
         ]));
         return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
     }
