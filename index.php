26c7327c04f8df22b7c3a46008134ad3bb0fdd61
joao.nogueira <joaonogueira20181@gmail.com>
2025-04-08 16:58:36 -0300

    save

--- index.php
+++ index.php
@@ -27,10 +27,9 @@ $errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (
         ->withStatus(404);
 });
  
-$app->get('tarefas', function (Request $request, Response $response, array $args) {
+$app->get('/tarefas', function (Request $request, Response $response, array $args) {
     $tarefa_service = new TarefaService();
     $tarefas = $tarefa_service->getAllTarefas();
-    $response->getBody()->write(json_encode($tarefas));
     $response->getBody()->write(json_encode($tarefas));
     return $response->withHeader('content-type', 'application/json');
 });
